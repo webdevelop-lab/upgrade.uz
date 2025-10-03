b89f4578f6552a10f90dd35a85958332c5966bb9
webdevelop-lab <websiter141@gmail.com>
2025-10-03 16:48:21 +0500

    upgrade.uz
    upgrade.uz

---
+++ Sayt/script.js
@@ -0,0 +1,12 @@
+alert("Assalomu aleykum UPGrade loyihasiga xush kelibsiz !!!")
+
+const name = prompt("Yoshingizni kiriting :");
+console.log(name);
+if(name >= 18){
+    console.log("kirish mumkin")
+    alert("Kirishingiz mumkin")
+}
+else{
+    console.log("Yoshingiz kichkina")
+    alert("Yoshingiz kichikina")
+}
